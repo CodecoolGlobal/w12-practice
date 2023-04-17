eb485ec01342f44bbe7eca1a4f034e6063247d66
Livia Dobai <livia.dobai@gmail.com>
2023-04-17 10:16:06 +0200

    initial commit to phase 1

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 9000
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/index.html`))
+})
+
+app.listen(port, () => {
+  console.log(`http://127.0.0.1:${port}`)
+})
