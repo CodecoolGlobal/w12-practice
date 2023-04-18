93e1551e0d58145a3bc67a90fab2c5aee3bf79f0
Livia Dobai <livia.dobai@gmail.com>
2023-04-18 12:03:55 +0200

    added phase3: upload image, delete image on BE and FE

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
+const fileUpload = require("express-fileupload");
+
 const app = express()
 const port = 9000
+
+app.use(fileUpload());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/index.html`))
@@ -13,6 +18,150 @@ app.use('/public', express.static(`${__dirname}/../frontend/public`))
 app.get('/images', (req, res) => {
   res.sendFile(path.join(`${__dirname}/data/images.json`))
 })
+
+app.get('/images/:id', (req, res) => {
+  console.log(req.params)
+  try {
+    const searchId = parseInt(req.params.id)
+    console.log(searchId)
+    if (isNaN(searchId)) {
+      console.log("nan")
+      res.status(418).send("NaN")
+    } else {
+      fs.readFile(`${__dirname}/data/images.json`, (err, data) => {// dirname nélkül azt veszi útvonalnak ahol álltunk amikor indítottuk a backendet
+        if (err) {
+          console.log(err)
+          res.send(err)
+        } else {
+          let result
+          const fileData = JSON.parse(data) // átalakítás nélül egy buffert ad vissza
+          console.log(fileData)
+          fileData.forEach((element, idx) => {
+            if (element.id === searchId) {
+              result = element
+            }
+          })
+          if (!result) {
+            res.status(404).send("nincs adat ezzel az id-vel")
+          } else {
+            res.send(result)
+          }
+
+        }
+      })
+    }
+  } catch (error) {
+    res.send("hiba")
+  }
+})
+
+app.post("/upload-image", (req, res) => {
+  console.log("req", req.body)
+  // check for errors
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+  //kinyerjük a képet a requestbőől
+  const newImage = req.files.image;
+  /**The .mv() method takes a single argument, which is the destination path where the file should be moved to. After moving the file, the original file at the temporary path created by the middleware is deleted automatically. */
+  newImage.mv(`${__dirname}/../frontend/public/img/${newImage.name}`, err => {
+    if (err) {
+      console.log(err);
+    };
+  });
+//beolvassuk a json/t
+  fs.readFile(`${__dirname}/data/images.json`, (err, data) => {
+    //két paraméter, a dataát parsolni kell
+    if (err) {
+      res.send("reading error")
+    } else {
+      let imgData = JSON.parse(data);
+      // console.log("imgData",imgData)
+
+      // A dátumot kihagyhatjuk
+      // const date = new Date();
+      // const getUploadDate = `${date.getFullYear()}. ${date.getMonth()}. ${date.getDate()}`;
+      const lastId = imgData[imgData.length-1].id + 1
+      const newImageData = {
+        "id": lastId,
+        "url": `/${newImage.name}`,
+        "title": req.body.title,
+        // "uploadDate": getUploadDate,
+        "phName": req.body.phName,
+      };
+      // manipuláljuk a file eredeti tartalmát
+      imgData.push(newImageData);
+
+      //  beírjuk a file-t
+      fs.writeFile(`${__dirname}/data/images.json`, JSON.stringify(imgData, null, 4), (error) => {
+        if (error) {
+          res.send("writing error")
+        } else {
+          return res.json(newImageData);
+        };
+      });
+    }
+  })
+});
+
+app.delete("/delete-image/:id", (req, res) => {
+  const paramId = req.params.id;
+  console.log("paramId",paramId)
+
+  fs.readFile(`${__dirname}/data/images.json`, (err, data) => {
+      if(err) {
+          console.log(err)
+      } else {
+        // const dataToDelete = JSON.parse(data).filter(img => {
+          //   console.log("img", img)
+          //   img.id === paramId
+          // })
+          
+          let dataToDelete = null
+          const fileData = JSON.parse(data)
+          console.log("data",fileData)
+          for (let index = 0; index < fileData.length; index++) {
+            const element = fileData[index];
+            console.log("element",element)
+            console.log("elementid",element.id)
+
+            if(element.id === parseInt(paramId)) {
+              dataToDelete = element
+            }
+          }
+          
+          console.log("dataToDelete",dataToDelete)
+          const pictureUploadPath = `${__dirname}/../frontend/public/img${dataToDelete.url}`;
+          console.log("pictureUploadPath",pictureUploadPath)
+
+          if (fs.existsSync(pictureUploadPath)) {
+                fs.unlinkSync(pictureUploadPath, (err) => {
+                  if (err) {
+                    console.log(err);
+                    return res.status(500).send(err);
+                  }
+                });
+              }
+
+          const imgData = JSON.parse(data).filter(img => img.id != paramId);
+
+          fs.writeFile(`${__dirname}/data/images.json`, JSON.stringify(imgData, null, 4), (error) => {
+              if(error) {
+                  console.log(error);
+              } else {
+                  // res.json(imgData)
+                  // return res.send(imgData)
+
+              };
+          });
+      };
+      
+  });
+
+return res.status(200).send("done");
+});
+
+
 
 app.listen(port, () => {
   console.log(`http://127.0.0.1:${port}`)
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,17 +1,30 @@
 console.log("loaded")
 
-const imagesComponent = (url, title, uploadDate, phName) => `
+const imagesComponent = ({id, url, title, uploadDate, phName}) => `
     <div class="swiper-slide">    
-        <div class="card">
+        <div class="card" data-id="${id}">
             <h2 class="title">${title}</h2>
             <h3>${uploadDate}</h3>
             <h4>${phName}</h4>
+            <button class="delete-pic">Delete</button>
             <img src="public/img${url}" />
         </div>
     </div>
-`;
+`
 
-const swiperWrapElement = document.querySelector('.swiper-wrapper');
+const formComponent = () => `
+    <form id="add-image">
+        <input type="file" name="file" />
+        <br>
+        <label for="title">Title:</label>
+        <input type="text" name="title" id="title" />
+        <br>
+        <label for="ph-name">Photographer:</label>
+        <input type="text" name="ph-name" id="ph-name" />
+        <br>
+        <button id="send-button">Send</button>
+        </form>
+        `
 
 // initialize swiper
 const swiper = new Swiper('.swiper', {
@@ -36,25 +49,86 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
+const rootElement = document.querySelector('#root');
+const swiperWrapElement = document.querySelector('.swiper-wrapper');
 
-fetch(`http://127.0.0.1:9000/images`) // fontos, hogy itt az endpoint url-t adjuk meg
+// FETCH IMAGES
+fetch(`http://127.0.0.1:9000/images`) 
   .then(response => {
     if (response.status === 201) {
       console.log('ok')
     }
-    return response.json() // a.json() ayt csin'lja, hogz kicsomagolja nek-nk a nek-nk kellő adatokat, mert a .then-el sokkal több más jön
+    return response.json() 
   })
   .then(responseJson => {
     const data = responseJson
-    console.log(data)
     data.forEach(element => {
+      // alap megoldás:
       // document.querySelector("#root").insertAdjacentHTML("beforeend", 
       // `<h3>${element.title}</h3>
       // <br>
       //   <img src="/public/img${element.url}" />
       // `)
-      swiperWrapElement.insertAdjacentHTML('beforeend', imagesComponent(element.url, element.title, element.uploadDate, element.phName));
+      swiperWrapElement.insertAdjacentHTML('beforeend', imagesComponent(element));
+    })
   })
-})
+
+// ADD NEW IMAGE
+rootElement.insertAdjacentHTML('beforeend', formComponent());
+const formElement = document.querySelector('#add-image')
+
+formElement.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  let inputFile = formElement.querySelector('input[name="file"]').files[0]
+  let inputTitle = formElement.querySelector('input[name="title"]').value
+  let inputPhName = formElement.querySelector('input[name="ph-name"]').value
+  
+  if (inputTitle.length === 0 || inputPhName.length === 0) {
+    return alert('Please fill all the required fields!');
+  };
+  const formData = new FormData();
+  formData.append('image', inputFile);
+  formData.append('title', inputTitle);
+  formData.append('phName', inputPhName);
+  
+  
+  fetch('/upload-image', {
+    method: "POST",
+    body: formData
+  })
+    .then(response => response.json())
+    .then(responsejson => {
+      swiperWrapElement.insertAdjacentHTML('beforeend', imagesComponent(responsejson))
+      deleting()
+    })
+});
+
+// DELETE IMAGE
+const deleting = () => {
+  setTimeout(() => {
+    const deleteButtons = document.querySelectorAll('.delete-pic');
+    console.log("deleteButtons",deleteButtons.length)
+  
+    for (let i = 0; i < deleteButtons.length; i++) {
+        const button = deleteButtons[i];
+        console.log("button", button)
+        button.addEventListener("click", function deleteImage(e) {
+            const bulletPoint = document.querySelectorAll('.swiper-pagination-bullet')[i];
+            let imageDiv = e.target.parentElement;
+            console.log("imageDiv",imageDiv)
+            let identifier = imageDiv.dataset.id
+            console.log("identifier", identifier)
+
+            fetch(`/delete-image/${identifier}`, {
+                method: 'DELETE',
+            })
+                .then(imageDiv.parentElement.remove())
+                .then(bulletPoint.remove())
+        });
+    }
+  }, 1000)
+}
+deleting()
 
 
